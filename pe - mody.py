ac90093122f875fd87882a87e45f83acaf65d54b
izitu <nik1297@gmail.com>
2020-01-12 21:58:32 +0300

    качаем последнюю серию из многих

--- pe - mody.py
+++ pe - mody.py
@@ -3,14 +3,14 @@ import re
 import wget, os
 import json
 from urllib.request import urlopen
-from selenium import webdriver
+#from selenium import webdriver
 import time
-
+#series__link-block
 s0=requests.get('http://www.tvc.ru/channel/brand/id/14')
 b=bs4.BeautifulSoup(s0.text, "html.parser")
 
 # получаем url и скачиваем картинку
-if b.find('meta', property='og:title') is not None:
+if b.find('meta', property='og:video') is not None:
     ser_title = b.find('meta', property='og:title').get('content')
     print(ser_title)
 
@@ -23,8 +23,27 @@ if b.find('meta', property='og:title') is not None:
     video_url_raw = b.find('meta', property='og:video').get('content').replace('iframe','json')
     print(video_url_raw)
 else:
-    print('Ничего не выйдет!')
-    exit(0)
+    print('Ничего не выйдет! - это не первая и единственная серия в году')
+    # ищем последнюю серию
+    url_last_ser = 'http://www.tvc.ru' + b.find(class_='series__link-block').get('href')
+    print(url_last_ser)
+
+    s1 = requests.get(url_last_ser)
+    b = bs4.BeautifulSoup(s1.text, "html.parser")
+
+    ser_title = b.find('meta', property='og:title').get('content')
+    print(ser_title)
+
+    ser_anons = b.find('meta', property='og:description').get('content')
+    print(ser_anons)
+
+    img_url = b.find('meta', property='og:image').get('content')
+    print(img_url)
+
+    video_url_raw = b.find('meta', property='og:video').get('content').replace('iframe','json')
+    print(video_url_raw)
+
+    #exit(0)
 # качаем картинку
 filename = wget.download(img_url)
 os.rename(filename, u''+os.getcwd()+'/'+filename)
